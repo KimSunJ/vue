a420988731b250db77d748d4dc25bedb0bcd4c69
kimsj <sophia10047600@gmail.com>
2023-05-30 12:31:26 +0900

    add : vue start

---
+++ 0530_vue_start/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { createSSRApp } from 'vue';
+import { renderToString } from 'vue/server-renderer';
+
+const server = express();
+
+server.get('/', (req, res) => {
+    const app = createSSRApp();
+
+    renderToString(app).then((html) => {
+        res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Vue SSR Example</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+      </body>
+    </html>
+    `);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('ready');
+});
